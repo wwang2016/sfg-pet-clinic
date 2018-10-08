0e9e602657c39cdc1de02ca2ce3420e863adce1f
internet <test20101010test@gmail.com>
2018-10-08 10:00:33 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/sftpetclinic/SftPetClinicApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sftpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/guru/springframework/sftpetclinic/SftPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.sftpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SftPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
