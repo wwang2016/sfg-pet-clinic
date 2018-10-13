35f82a384701986d36d9770ff33309b108227bf3
wwang2016 <test20101010test@gmail.com>
2018-10-13 16:07:40 -0400

    Adding pojos. closes #1

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+
+
+    private String firsName;
+    private String lastName;
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
